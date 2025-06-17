c5f076e064ae96d95e6ed5d9e2e595e77029d343
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-06-17 21:20:32 +0530

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,20 @@
+﻿namespace Factoirial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+
+            long factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"\nFactorial of {num} is: {factorial}");
+        }
+    }
+}
