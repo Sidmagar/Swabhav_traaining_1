66daf7dd0ba2f9ead6b1ddb122c8cd835e889d6d
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-06-17 21:49:31 +0530

    Add files via upload

---
+++ Count_words.cs
@@ -0,0 +1,15 @@
+﻿namespace Count_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            Console.WriteLine("Total words: " + words.Length);
+        }
+    }
+}
