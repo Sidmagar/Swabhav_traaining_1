afc1e4c505920d8a4649d85eee2367c9eff2cb96
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-06-24 21:59:53 +0530

    Add files via upload

---
+++ Dayofweekenum.cs
@@ -0,0 +1,32 @@
+﻿namespace Dayofweekenum
+{
+    internal class Dayofweekenum
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number (1-7): ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int dayNumber) && dayNumber >= 1 && dayNumber <= 7)
+            {
+                Day selectedDay = (Day)dayNumber;
+                Console.WriteLine($"\nDay is: {selectedDay}");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid input. Please enter a number between 1 and 7.");
+            }
+        }
+    }
+
+    public enum Day
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
