c16ef5a107540e60c835057f38b7c0314330ab16
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-06-28 21:37:10 +0530

    Add files via upload

---
+++ Products/Product/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProductApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(101, "Wireless Mouse", 800, 10);
+            Product product2 = new Product(102, "Bluetooth Speaker", 1500);
+
+            Console.WriteLine("Product Details:\n");
+            product1.DisplayDetails();
+            product2.DisplayDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
